1ee4db6d5998fb3960a86fa2ae408d77f1d1e556
Justin Bush <jaybush7@gmail.com>
2017-01-18 12:21:10 -0500

    Initial commit

---
+++ CodeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CodeViewController.swift
+//  SquareCode
+//
+//  Created by Justin Bush on 2017-01-18.
+//  Copyright © 2017 Justin Bush. All rights reserved.
+//
+
+import UIKit
+
+class CodeViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
